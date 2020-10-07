6368b9311b83320e7798c229ca04883a0349fd30
Abhinav1281 <abhisione2@gmail.com>
2020-10-07 22:19:00 +0530

    Changed the animation timing from 1000ms to 400ms to reduce the delay between the animation. Also set the image to be non-clickable after being once clicked.
    Signed-off-by: Abhinav1281 <abhisione2@gmail.com>

--- app/src/main/java/com/example/tictactoe/Playable.java
+++ app/src/main/java/com/example/tictactoe/Playable.java
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Playable extends AppCompatActivity {
 
@@ -41,13 +43,13 @@ public class Playable extends AppCompatActivity {
         int tag = Integer.parseInt(image.getTag().toString());
         if(isClicked(tag))
         {
-
+            Toast.makeText(this,"Already pressed",Toast.LENGTH_SHORT).show();
         }
         else {
             image.setTranslationX(-2000);
             if (player.equals(getString(R.string.player1))) {
                 image.setImageResource(R.drawable.circle);
-                image.animate().translationXBy(2000).rotationY(3600).alpha(1).setDuration(1000);
+                image.animate().translationXBy(2000).rotationY(3600).alpha(1).setDuration(400);
                 player = getString(R.string.player2);
                 playerName.setText("PLAYER'S CHANCE:"+player2);
                 pdisplay=player1;
@@ -55,24 +57,26 @@ public class Playable extends AppCompatActivity {
                 playerChoice[tag] = R.string.player1;
             } else if (player.equals(getString(R.string.player2))) {
                 image.setImageResource(R.drawable.cross);
-                image.animate().translationXBy(2000).rotationY(3600).alpha(1).setDuration(1000);
+                image.animate().translationXBy(2000).rotationY(3600).alpha(1).setDuration(400);
                 player = getString(R.string.player1);
                 playerName.setText("PLAYER'S CHANCE:"+player1);
                 pdisplay=player2;
                 playerChoice[tag] = R.string.player2;
             }
-        }
-        boolean win=checkWinner();
-        boolean isVacant=isVacant();
-        if(win)
-        {
-            showWinAlert(pdisplay);
+            boolean win=checkWinner();
+            boolean isVacant=isVacant();
+            if(win)
+            {
+                showWinAlert(pdisplay);
+            }
+
+            else if(isVacant)
+            {
+                showWinAlert("NO ONE");
+            }
+            image.setClickable(false);
         }
 
-        else if(isVacant)
-        {
-            showWinAlert("NO ONE");
-        }
 
     }
     private boolean checkWinner()
